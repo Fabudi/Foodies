58f7f6fa66faf6d5550a8857427eb4a9ad740473
Fabudi <evgeniydodg@gmai.com>
2024-05-25 20:36:40 +0300

    Adding Item in Cart functions

--- app/src/main/java/inc/fabudi/foodies/viewmodel/MainViewModel.kt
+++ app/src/main/java/inc/fabudi/foodies/viewmodel/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import inc.fabudi.foodies.data.CartState
+import inc.fabudi.foodies.data.Product
 import inc.fabudi.foodies.network.ApiService
 import inc.fabudi.foodies.network.ApiState
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -52,6 +53,60 @@ class MainViewModel @Inject constructor(private val apiService: ApiService) : Vi
                 categoriesState.value = ApiState.Error("Failed to fetch data")
             }
         }
+    }
+
+    fun addItem(productId: Int): Int {
+        val products = ((productsState.value as ApiState.Success).data as List<Product>)
+        val product = products.find { it.id == productId } ?: products[0]
+        if (cartState.value is CartState.Empty) {
+            cartState.value = CartState.Filled(setOf(Pair(product, 1)))
+            return 1
+        }
+
+        val state = cartState.value as CartState.Filled
+        val cartProduct = state.products.find { it.first == product }
+        if (cartProduct == null) {
+            cartState.value = CartState.Filled(state.products.plus(setOf(Pair(product, 1))))
+            return 1
+        }
+
+        cartState.value = CartState.Filled(
+            state.products.minus(cartProduct).plus(
+                cartProduct.copy(
+                    second = cartProduct.second + 1
+                )
+            )
+        )
+        return cartProduct.second + 1
+    }
+
+    fun removeItem(productId: Int): Int {
+        val product =
+            ((productsState.value as ApiState.Success).data as List<Product>).find { it.id == productId }
+        if (cartState.value is CartState.Empty) return 0
+
+        val state = cartState.value as CartState.Filled
+        val cartProduct = state.products.find { it.first == product }
+        if (cartProduct == null) return 0
+
+        val newItemCounter = cartProduct.second - 1
+        if (state.products.size == 1 && newItemCounter == 0) {
+            cartState.value = CartState.Empty
+            return 0
+        }
+        if (newItemCounter == 0) {
+            cartState.value = CartState.Filled(state.products.minus(cartProduct))
+            return 0
+        }
+
+        cartState.value = CartState.Filled(
+            state.products.minus(cartProduct).plus(
+                cartProduct.copy(
+                    second = newItemCounter
+                )
+            )
+        )
+        return newItemCounter
     }
 
 }
