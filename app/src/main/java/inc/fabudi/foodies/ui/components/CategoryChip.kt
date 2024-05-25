24d2172ecee41e0cdb48cf1721fffbc1637a9dc6
Fabudi <evgeniydodg@gmai.com>
2024-05-25 20:31:02 +0300

    Implementing Category selector

---
+++ app/src/main/java/inc/fabudi/foodies/ui/components/CategoryChip.kt
@@ -0,0 +1,45 @@
+package inc.fabudi.foodies.ui.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import inc.fabudi.foodies.ui.theme.HighEmphasis
+
+@Composable
+fun CategoryChip(
+    modifier: Modifier = Modifier,
+    text: String = "Category",
+    isSelected: Boolean = true,
+    onClick: () -> Unit = {}
+) {
+    FilledTonalButton(
+        modifier = modifier,
+        onClick = onClick,
+        shape = RoundedCornerShape(8.dp),
+        contentPadding = PaddingValues(16.dp, 12.dp, 16.dp, 12.dp),
+        colors = ButtonDefaults.elevatedButtonColors(
+            containerColor = if (isSelected) MaterialTheme.colorScheme.primary else Color.Transparent,
+            contentColor = if (isSelected) MaterialTheme.colorScheme.onPrimary else HighEmphasis
+        )
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium,
+            color = if (isSelected) MaterialTheme.colorScheme.onPrimary else HighEmphasis
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun CategoryChipPreview() {
+    CategoryChip()
+}
---
+++ app/src/main/java/inc/fabudi/foodies/ui/components/CategorySelector.kt
@@ -0,0 +1,60 @@
+package inc.fabudi.foodies.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import inc.fabudi.foodies.data.Category
+import kotlinx.coroutines.launch
+
+@Composable
+fun CategorySelector(
+    modifier: Modifier = Modifier,
+    categories: List<Category> = mutableListOf(),
+    onClick: (id: Int) -> Unit = {}
+) {
+    var selected by remember {
+        mutableStateOf("")
+    }
+    val lazyListState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    LazyRow(
+        state = lazyListState,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(start = 16.dp, end = 16.dp),
+    ) {
+        itemsIndexed(categories) { index, category ->
+            CategoryChip(
+                text = category.name,
+                isSelected = selected.contains(category.name),
+                onClick = {
+                    coroutineScope.launch {
+                        lazyListState.animateScrollToItem(index)
+                    }
+                    selected = if (selected == category.name) {
+                        onClick(0)
+                        ""
+                    } else {
+                        onClick(category.id)
+                        category.name
+                    }
+                })
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CategorySelectorPreview() {
+    CategorySelector()
+}
