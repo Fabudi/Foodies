98a54b1c9aeaa85c7d4250bfc9520dc76931d3b9
Fabudi <evgeniydodg@gmai.com>
2024-05-26 01:21:18 +0300

    Adding more destinations

--- app/src/main/java/inc/fabudi/foodies/ui/navigation/Destinations.kt
+++ app/src/main/java/inc/fabudi/foodies/ui/navigation/Destinations.kt
@@ -2,4 +2,7 @@ package inc.fabudi.foodies.ui.navigation
 
 sealed class Destination(val route: String) {
     data object Home : Destination("home")
+    data object Search : Destination("search")
+    data object Details : Destination("details")
+    data object Cart : Destination("details")
 }
