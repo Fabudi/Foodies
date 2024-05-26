35af6abaa7b79d89ac722ab3b2b8de9b909e1529
Fabudi <evgeniydodg@gmai.com>
2024-05-26 13:11:49 +0300

    Fix extra padding

--- app/src/main/java/inc/fabudi/foodies/ui/components/TopBar.kt
+++ app/src/main/java/inc/fabudi/foodies/ui/components/TopBar.kt
@@ -1,6 +1,7 @@
 package inc.fabudi.foodies.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,13 +11,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,45 +39,51 @@ fun TopBar(
     searchOnClick: () -> Unit = {},
     categoryOnClick: (Int) -> Unit = {}
 ) {
-    Column(
-        modifier = modifier
-            .systemBarsPadding()
-            .displayCutoutPadding()
-            .padding(top = 16.dp, start = 8.dp, end = 8.dp)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+    Box(modifier = Modifier.wrapContentSize()){
+        Column(
+            modifier = modifier
+                .shadow(4.dp)
+                .background(MaterialTheme.colorScheme.background)
+                .padding(top = 16.dp, start = 8.dp, end = 8.dp, bottom = 8.dp)
+                .displayCutoutPadding()
         ) {
-            Box {
-                IconButton(onClick = filterOnClick, modifier = Modifier.size(44.dp)) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box {
+                    IconButton(onClick = filterOnClick, modifier = Modifier.size(44.dp)) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.filter),
+                            contentDescription = "Filter button"
+                        )
+                    }
+                    if (filterCounter > 0)
+                        FilterCounter(
+                            value = filterCounter,
+                            modifier = Modifier.align(Alignment.TopEnd)
+                        )
+                }
+                Image(
+                    painterResource(id = R.drawable.logo),
+                    contentDescription = "App logo image",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.height(44.dp)
+                )
+                IconButton(onClick = searchOnClick, modifier = Modifier.size(44.dp)) {
                     Icon(
-                        painter = painterResource(id = R.drawable.filter),
-                        contentDescription = "Filter button"
+                        painter = painterResource(id = R.drawable.search),
+                        contentDescription = "Search button"
                     )
                 }
-                if(filterCounter>0)
-                    FilterCounter(value = filterCounter, modifier = Modifier.align(Alignment.TopEnd))
             }
-            Image(
-                painterResource(id = R.drawable.logo),
-                contentDescription = "App logo image",
-                contentScale = ContentScale.Fit,
-                modifier = Modifier.height(44.dp)
-            )
-            IconButton(onClick = searchOnClick, modifier = Modifier.size(44.dp)) {
-                Icon(
-                    painter = painterResource(id = R.drawable.search),
-                    contentDescription = "Search button"
+            if (apiState is ApiState.Success) {
+                CategorySelector(
+                    categories = apiState.data as List<Category>,
+                    onClick = categoryOnClick
                 )
             }
-        }
-        if (apiState is ApiState.Success) {
-            CategorySelector(
-                categories = apiState.data as List<Category>,
-                onClick = categoryOnClick
-            )
         }
     }
 }
